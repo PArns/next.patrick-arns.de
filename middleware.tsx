2a4a892aff71170ba668fdc2407221f6469f24cb
Patrick Arns <git@patrick-arns.de>
2023-12-31 15:29:44 +0100

    Exclude non needed calls from middleware

--- middleware.tsx
+++ middleware.tsx
@@ -7,3 +7,17 @@ export async function middleware(request: NextRequest) {
     },
   });
 }
+
+// the following code has been copied from https://nextjs.org/docs/advanced-features/middleware#matcher
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
