7908458b455b35770defbe97bde53f157643dd00
Patrick Arns <git@patrick-arns.de>
2024-02-11 14:15:41 +0100

    Fixed build error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,7 +13,7 @@ export default async function NotFound() {
               Page not found
             </h1>
             <p className="mt-4 text-gray-500 dark:text-gray-400">
-              Sorry, the page you are looking for doesn't exist.
+              Sorry, the page you are looking for doesn&apos;t exist.
             </p>
 
             <div className="flex items-center mt-6 gap-x-3">
