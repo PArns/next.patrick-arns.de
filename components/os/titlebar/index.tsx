f8e853e00327beb9b7fb5612ba470cac6941a763
Patrick Arns <github@patrick-arns.de>
2024-02-25 12:52:09 +0100

    Fixed title text on small resolutions

--- components/os/titlebar/index.tsx
+++ components/os/titlebar/index.tsx
@@ -29,7 +29,7 @@ export default function TitleBar({ pageName }: { pageName: string }) {
 
   return (
     <div className="flex flex-row bg-white/50 px-2 drop-shadow backdrop-blur-lg">
-      <div className="flex-none">
+      <div className="w-max flex-none">
         <div className="flex flex-row">
           <div>
             <Image
@@ -40,7 +40,7 @@ export default function TitleBar({ pageName }: { pageName: string }) {
               className="pr-1 pt-1 drop-shadow-[0_0.8px_0.8px_rgba(0,0,0,0.8)]"
             />
           </div>
-          <div>{title}</div>
+          <div className="truncate w-60 md:w-auto">{title}</div>
         </div>
       </div>
       <div className="flex-grow"></div>
