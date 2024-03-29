5c83000e571a2624c3c1102a3880476dbf1448d7
Patrick Arns <github@patrick-arns.de>
2024-03-29 01:37:31 +0100

    Added blog redirect

---
+++ app/(desktop)/[lng]/@blog/(.)/blog/[...catchAll]/page.tsx
@@ -0,0 +1,16 @@
+import { GetBlogPostBySlug } from "@/contentful/provider/blog-post-provider";
+import { permanentRedirect, notFound } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+export default async function DefaultRedirect({
+  params,
+}: {
+  params: { catchAll: string; lng: string };
+}) {
+  const post = await GetBlogPostBySlug(params.catchAll, params.lng);
+
+  if (post === null) notFound();
+
+  revalidatePath(`/${params.lng}/blog/${params.catchAll}`);
+  permanentRedirect(`/${params.lng}/blog/article/${params.catchAll}`);
+}
