ac8c47fb246ed326983c74d130ecce15f3505a1c
Patrick Arns <github@patrick-arns.de>
2025-03-27 22:16:29 +0100

    build info fix

--- components/os/build-number/index.tsx
+++ components/os/build-number/index.tsx
@@ -1,13 +1,12 @@
 import DateRenderer from "@/components/date-renderer";
 import fs from "fs";
-import path from "path";
 import React from "react";
+import buildInfo from "../../../build-info.json";
 
 export const dynamic = "force-static";
 
 export default function BuildInfo() {
-  const buildInfoPath = path.resolve(process.cwd(), "build-info.json");
-  const buildInfo = JSON.parse(fs.readFileSync(buildInfoPath, "utf8"));
+  if (!buildInfo) return null;
   const buildDate = new Date(buildInfo.buildDate);
 
   return (
