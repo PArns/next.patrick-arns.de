787ec7de2930f65095675f188754d49c2225047e
Patrick Arns <github@patrick-arns.de>
2025-03-27 22:16:44 +0100

    Code cleanup

--- components/os/build-number/index.tsx
+++ components/os/build-number/index.tsx
@@ -1,5 +1,4 @@
 import DateRenderer from "@/components/date-renderer";
-import fs from "fs";
 import React from "react";
 import buildInfo from "../../../build-info.json";
 
@@ -12,7 +11,11 @@ export default function BuildInfo() {
   return (
     <div className="flex items-center">
       <span className="text-xs text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-        <span>Version: </span>{buildInfo.buildNumber} <span className="hidden md:inline">(<DateRenderer date={buildDate} />)</span>
+        <span>Version: </span>
+        {buildInfo.buildNumber}{" "}
+        <span className="hidden md:inline">
+          (<DateRenderer date={buildDate} />)
+        </span>
       </span>
     </div>
   );
