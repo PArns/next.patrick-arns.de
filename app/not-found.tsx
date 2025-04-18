9bfcbabd3828576873d52c090838463cb27191ee
Patrick Arns <github@patrick-arns.de>
2025-04-18 02:41:27 +0200

    Middleware optimizations and default 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -41,13 +41,6 @@ export default function NotFound() {
 
                 <span>Go Home</span>
               </Link>
-
-              <Link
-                href="/blog"
-                className="w-1/2 shrink-0 rounded-lg bg-blue-500 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-500 sm:w-auto"
-              >
-                Blog
-              </Link>
             </div>
           </div>
 
