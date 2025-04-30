be5b28ff9f90f2765d88340f95485f541e8970d5
Patrick Arns <github@patrick-arns.de>
2025-05-01 00:28:30 +0200

    Blog header image adjustment

--- components/blog/blog-header/index.tsx
+++ components/blog/blog-header/index.tsx
@@ -21,8 +21,8 @@ export default function BlogHeader({
         usePlaceholder={true}
         className="absolute top-0 right-0 bottom-0 left-0 h-max w-max object-cover"
       />
-      <div className="py-10 @md/header:py-20 @lg/header:py-28">
-        <div className="flex h-full items-center justify-center">
+      <div className="relative py-8 @md/header:py-16 @2xl/header:py-32">
+        <div className="flex items-center justify-center">
           <div className="z-10 rounded-xl border-neutral-500 bg-white/30 p-5 text-white backdrop-blur-md dark:border-neutral-600 dark:bg-neutral-800/50">
             <h1 className="mb-2 text-4xl font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] @md/header:text-5xl @lg/header:text-6xl">
               {title}
